0bc07f2845673d5bc5ff113f1929c7659db0aad0
Simon Huang <Simon Huang>
2017-07-13 18:10:16 -0400

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/',function(req, res) {
 app.get('/*', function(req, res) {
     var url = req.path.substring(1);
     for (var i = 0; i < ROOM_LIST.length; i++) {
-        if (ROOM_LIST[i].id == url && ROOM_LIST[i].count < 4) {
+        if (ROOM_LIST[i].id == url && ROOM_LIST[i].count < 4 && ROOM_LIST[i].hasStarted == false) {
             res.sendFile(__dirname + '/index.html');
             return;
         }
@@ -57,66 +57,54 @@ var Room = function(id) {
         id : id,
         count : 0,
         winCount : 0,
-        refresh : 0
+        refresh : 0,
+        hasStarted : false
     }
     return self;
 }
 
+var running = true;
 var runtime = setInterval(function() {
-    var pack = [];
-    for(var i in PLAYER_LIST) {
-        var player = PLAYER_LIST[i];
-        player.updatePosition();
-        pack.push({
-            x : player.x,
-            y : player.y,
-            color : player.color,
-            room : player.room,
-            key : player.key,
-        });
-    }
-    for(var i in SOCKET_LIST) {
-        var socket = SOCKET_LIST[i];
-        socket.emit('collision', {player : PLAYER_LIST[socket.id]});
-        socket.emit('newPositions', pack);
-    }
+    updatePos();
 }, 1000/40);
 
 io.on('connection', function(socket) {
- 
+
+    // actions for disconnection from a room
     socket.on('disconnect',function(){
         if (PLAYER_LIST[socket.id] != null) {
             var player = PLAYER_LIST[socket.id];
             for (var i = 0; i < ROOM_LIST.length; i++) {
                 if (player.room == ROOM_LIST[i].id) {
+                    ROOM_LIST[i].count--;
                     
-                    if (ROOM_LIST[i].count == 4) {
+                    if (ROOM_LIST[i].hasStarted) {
                         delete PLAYER_LIST[socket.id];
                         delete SOCKET_LIST[socket.id];
                         if (player.canMove && ROOM_LIST[i].winCount < 3)
                             ROOM_LIST[i].winCount++;
                         if (ROOM_LIST[i].winCount == 3) {
-                            var winner = getWinner(ROOM_LIST[i].id);
-                            io.to(ROOM_LIST[i].id).emit('winner', {winner : winner.username, color : winner.color});
+                            var winner = getWinner(ROOM_LIST[i]);
+                            io.to(ROOM_LIST[i].id).emit('winner', {winner : winner.username, color : winner.color, count : ROOM_LIST[i].count});
+                            running = false;
+                            setTimeout(function(){ running = true; io.to(player.room).emit('clearCanvas', {}); }, 3000);
+                            initPos(player);
                             ROOM_LIST[i].winCount = 4;
+                        } else {
+                            for (var j in PLAYER_LIST) {
+                                if (PLAYER_LIST[j].room == player.room && player.count < PLAYER_LIST[j].count)
+                                    PLAYER_LIST[j].count--;
+                            }
                         }
-                        ROOM_LIST[i].count--;
-                        break;
+                    } else {
+                        if (ROOM_LIST[i].count == 0)
+                            ROOM_LIST.splice(i, 1);
+                        io.to(player.room).emit('clearCanvas', {});
+                        delete PLAYER_LIST[socket.id];
+                        delete SOCKET_LIST[socket.id];
+                        initPos(player);
                     }
-
-                    ROOM_LIST[i].count--;
-                    if (ROOM_LIST[i].count == 0)
-                        ROOM_LIST.splice(i, 1);
-                    io.to(player.room).emit('clearCanvas', {});
-                    delete PLAYER_LIST[socket.id];
-                    delete SOCKET_LIST[socket.id];
-                    for (var i in PLAYER_LIST) {
-                        if (PLAYER_LIST[i].room == player.room && player.count < PLAYER_LIST[i].count) {
-                            PLAYER_LIST[i].count--;
-                            getPosInRoom(PLAYER_LIST[i]);
-                            getColor(PLAYER_LIST[i]);
-                        }
-                    }
+                    
                     io.to(player.room).emit('getUsers', {players : PLAYER_LIST});
                     break;
                 }
@@ -124,6 +112,7 @@ io.on('connection', function(socket) {
         }
     });
     
+    // update rooms (for lobby)
     socket.emit('getRooms', {rooms : ROOM_LIST});
     socket.on('updateRooms', function(data) {
         socket.emit('getRooms', {rooms : ROOM_LIST});
@@ -164,6 +153,7 @@ io.on('connection', function(socket) {
                             PLAYER_LIST[j].canMove = true;
                         }
                     }
+                    startGame(ROOM_LIST[i]);
                 }
                 break;
             }
@@ -171,7 +161,7 @@ io.on('connection', function(socket) {
         io.to(player.room).emit('getUsers', {players : PLAYER_LIST});
     });
     
-    // check if a room exists
+    // check if a room exists (for lobby)
     socket.on('checkRoom', function(data) {
         var exists = false;
         for (var i = 0; i < ROOM_LIST.length; i++) {
@@ -183,6 +173,7 @@ io.on('connection', function(socket) {
         socket.emit('roomExists', {state : exists});
     });
     
+    // actions on player collision/death
     socket.on('crash', function(data) {
         var player = PLAYER_LIST[data.player];
         if (player.canMove) {
@@ -191,8 +182,8 @@ io.on('connection', function(socket) {
                 if (player.room == ROOM_LIST[i].id) {
                     ROOM_LIST[i].winCount++;
                     if (ROOM_LIST[i].winCount == 3) {
-                        var winner = getWinner(ROOM_LIST[i].id);
-                        io.to(ROOM_LIST[i].id).emit('winner', {winner : winner.username, color : winner.color});
+                        var winner = getWinner(ROOM_LIST[i]);
+                        io.to(ROOM_LIST[i].id).emit('winner', { winner : winner.username, color : winner.color, count : ROOM_LIST[i].count });
                         ROOM_LIST[i].winCount = 4;
                     }
                     break;
@@ -201,30 +192,49 @@ io.on('connection', function(socket) {
         }
     });
    
+    // send messages
     socket.on('message', function(data) {
         io.to(data.ROOM_ID).emit('showMsg', {MESSAGE : data.MESSAGE, USER_ID: data.USER_ID});
     });
     
+    // actions on clicking 'New Game' button
     socket.on('refresh', function(data) {
         for (var i = 0; i < ROOM_LIST.length; i++) {
             if (data.ROOM_ID == ROOM_LIST[i].id) {
                 ROOM_LIST[i].refresh++;
-                io.to(data.ROOM_ID).emit('newGame', {count : ROOM_LIST[i].refresh});
-                console.log(ROOM_LIST[i].refresh);
+                io.to(data.ROOM_ID).emit('updateRefresh', {count : ROOM_LIST[i].refresh});
+                if (ROOM_LIST[i].refresh == 4)
+                    socket.emit('newGame', {count : ROOM_LIST[i].refresh});
                 break;
             }
         }
     });
     
+    // reset the game
     socket.on('resetGame', function(data) {
         for (var i = 0; i < ROOM_LIST.length; i++) {
             if (data.ROOM_ID == ROOM_LIST[i].id) {
                 ROOM_LIST[i].refresh = 0;
                 ROOM_LIST[i].winCount = 0;
                 io.to(data.ROOM_ID).emit('clearCanvas', {});
-                getPosInRoom(PLAYER_LIST[socket.id]);
-                PLAYER_LIST[socket.id].canMove = true;
-                break;
+                
+                if (ROOM_LIST[i].count == 4) {
+                    for (var j in PLAYER_LIST) {
+                        if (PLAYER_LIST[j].room == data.ROOM_ID) {
+                            getPosInRoom(PLAYER_LIST[j]);
+                            PLAYER_LIST[j].canMove = true;
+                        }
+                    }
+                    startGame(ROOM_LIST[i]);
+                } else {
+                    for (var j in PLAYER_LIST) {
+                        if (PLAYER_LIST[j].room == data.ROOM_ID) {
+                            getPosInRoom(PLAYER_LIST[j]);
+                            getColor(PLAYER_LIST[j]);
+                        }
+                    }
+                    socket.emit('getUsers', {players : PLAYER_LIST});
+                }
             }
         }
     });
@@ -265,12 +275,61 @@ function getPosInRoom(player) {
 function getWinner(room) {
     var winner;
     for (var i in PLAYER_LIST) {
-        if (PLAYER_LIST[i].room == room && PLAYER_LIST[i].canMove) {
+        if (PLAYER_LIST[i].room == room.id && PLAYER_LIST[i].canMove) {
             winner = PLAYER_LIST[i];
             PLAYER_LIST[i].canMove = false;
         }
     }
+    room.hasStarted = false;
     return winner;
+}
+
+function startGame(room) {
+    updatePos();
+    running = false;
+    setTimeout(function(){ running = true; }, 5000);
+    var countdown = 5;
+    io.to(room.id).emit('countDown', {countdown : countdown});
+    var timer = setInterval(function() {
+        countdown--;
+        if (countdown == 0) {
+            clearInterval(timer);
+        }
+        io.to(room.id).emit('countDown', {countdown : countdown});
+    }, 1000);
+    room.hasStarted = true;
+}
+
+function initPos(player) {
+    for (var i in PLAYER_LIST) {
+        if (PLAYER_LIST[i].room == player.room && player.count < PLAYER_LIST[i].count) {
+            PLAYER_LIST[i].count--;
+            getPosInRoom(PLAYER_LIST[i]);
+            getColor(PLAYER_LIST[i]);
+        }
+    }
+}
+
+function updatePos() {
+    if (running) {
+        var pack = [];
+        for(var i in PLAYER_LIST) {
+            var player = PLAYER_LIST[i];
+            player.updatePosition();
+            pack.push({
+                x : player.x,
+                y : player.y,
+                color : player.color,
+                room : player.room,
+                key : player.key,
+            });
+        }
+        for(var i in SOCKET_LIST) {
+            var socket = SOCKET_LIST[i];
+            socket.emit('collision', {player : PLAYER_LIST[socket.id]});
+            socket.emit('newPositions', pack);
+        }
+    }    
 }
 
 http.listen(app.get('port'), function(){
